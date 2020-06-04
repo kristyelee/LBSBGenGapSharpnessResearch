4dcb0d1b9e31f7790eb40f9b7125e95e63e5d57f
Kristy Lee <kristylee@berkeley.edu>
2020-06-03 23:36:17 -0700

    C2, C4 net

---
+++ deepnet.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+F = nn.functional
+__all__ = ['cifar10_deepnet', 'cifar100_deepnet']
+
+class VGG(nn.module):
+    def __init__(self, num_classes=10):
+        super(VGG, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Conv2d(64, 64, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(128, 128, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(256, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(256, 256, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Conv2d(512, 512, kernel_size=3, padding = 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 512, bias=False),
+            nn.Dropout(0.5),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, 64 * 7 * 7)
+        x = self.classifier(x)
+        return F.log_softmax(x)
